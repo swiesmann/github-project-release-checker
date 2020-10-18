f327890c982a729285c739566e750e57bc05fc13
Simon Wiesmann <simon@swiesmann.de>
2020-10-18 15:14:25 +0200

    add output as json

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,27 @@
-fun main() {
-    println("::set-output name=releases_as_json::test")
-    println("::set-env name=releases_as_json::test")
+import java.io.File
+import java.net.URL
+import java.util.*
+import java.util.stream.Collectors
+
+//const val DATA_DIR = "./.github-project-release-checker"
+
+fun main(args: Array<String>) {
+
+    println("::set-output name=releases_as_json::{'hello':'world'}")
+
+    if (args.isEmpty()) {
+        print("no project URLs present. bye.")
+        return
+    }
+
+//    val dataDir = File(DATA_DIR)
+//    dataDir.mkdirs()
+//
+//    val githubService = GithubService()
+//    val releases = Arrays.stream(args)
+//        .map { URL(it) }
+//        .map { githubService.getLatestRelease(it) }
+//        .collect(Collectors.toList())
+//
+//    println(releases.toString())
 }
